dc0eaf8409313ebecebdd039916e53d85b65a875
itosha <anton.jimson@gmail.com>
2021-10-08 16:17:34 +0300

    test_commit

---
+++ Game_ball/Game_1.py
@@ -0,0 +1,40 @@
+import pygame
+from pygame.draw import *
+from random import randint
+pygame.init()
+
+FPS = 2
+screen = pygame.display.set_mode((1200, 900))
+
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+BLACK = (0, 0, 0)
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
+def new_ball():
+    '''рисует новый шарик '''
+    x = randint(100, 1100)
+    y = randint(100, 900)
+    r = randint(10, 100)
+    color = COLORS[randint(0, 5)]
+    circle(screen, color, (x, y), r)
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+    new_ball()
+    pygame.display.update()
+    screen.fill(BLACK)
+
+pygame.quit()
